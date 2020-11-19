a0a62e13560b9868b8b4890c6c03ae232f45366a
fcomuller <franciscomullertroncoso@gmail.com>
2020-11-19 17:31:21 -0300

    Add files via upload

---
+++ VeciHelpSirena.ino
@@ -0,0 +1,114 @@
+#include "FirebaseESP8266.h"
+#include <ESP8266WiFi.h>
+
+#define FIREBASE_HOST "vecihelpapk.firebaseio.com" //Without http:// or https:// schemes
+#define FIREBASE_AUTH "CnijfRVlrYCQkQ2LZY2RdeMsH74MYy2eQ96zydjN"
+#define WIFI_SSID "muller"
+#define WIFI_PASSWORD "manfredd"
+
+//Define FirebaseESP8266 data object
+FirebaseData firebaseData;
+
+
+unsigned long sendDataPrevMillis = 0;
+
+String path = "/Comunidad/id_Comunidad/Estado_Sirena";
+
+
+
+void printResult(FirebaseData &data);
+
+void setup()
+{
+  pinMode(4, OUTPUT);
+  Serial.begin(115200);
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.reconnectWiFi(true);
+
+  //Set the size of WiFi rx/tx buffers in the case where we want to work with large data.
+  firebaseData.setBSSLBufferSize(1024, 1024);
+
+  //Set the size of HTTP response buffers in the case where we want to work with large data.
+  firebaseData.setResponseSize(1024);
+
+  if (!Firebase.beginStream(firebaseData, path))
+  {
+    Serial.println("------------------------------------");
+    Serial.println("Can't begin stream connection...");
+    Serial.println("REASON: " + firebaseData.errorReason());
+    Serial.println("------------------------------------");
+    Serial.println();
+  }
+}
+
+void loop()
+{
+  
+
+
+  if (!Firebase.readStream(firebaseData))
+  {
+    Serial.println("------------------------------------");
+    Serial.println("Can't read stream data...");
+    Serial.println("REASON: " + firebaseData.errorReason());
+    Serial.println("------------------------------------");
+    Serial.println();
+  }
+
+  if (firebaseData.streamTimeout())
+  {
+    Serial.println("Stream timeout, resume streaming...");
+    Serial.println();
+  }
+
+  if (firebaseData.streamAvailable())
+  {
+    Serial.println("------------------------------------");
+    Serial.println("Stream Data available... Printing");
+    Serial.println("STREAM PATH: " + firebaseData.streamPath());
+    Serial.println("EVENT PATH: " + firebaseData.dataPath());
+    Serial.println("DATA TYPE: " + firebaseData.dataType());
+    Serial.println("EVENT TYPE: " + firebaseData.eventType());
+    Serial.print("VALUE: ");
+    printResult(firebaseData);
+    if(firebaseData.intData()== 1){
+      digitalWrite(4,HIGH);
+      }
+      else{
+        digitalWrite(4,LOW);
+        }
+    Serial.println("------------------------------------");
+    Serial.println();
+  }
+
+  
+}
+
+void printResult(FirebaseData &data)
+{
+  
+  if (data.dataType() == "int")
+    Serial.println(data.intData());
+  else if (data.dataType() == "float")
+    Serial.println(data.floatData(), 5);
+  else if (data.dataType() == "double")
+    printf("%.9lf\n", data.doubleData());
+  else if (data.dataType() == "boolean")
+    Serial.println(data.boolData() == 1 ? "true" : "false");
+  else if (data.dataType() == "string")
+    Serial.println(data.stringData());
+
+}
